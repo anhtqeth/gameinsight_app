ddb59fbc0b2688783b1e6191fe33cf326078ea73
ethudev <anh.tq2411ps@gmail.com>
2019-05-29 05:35:57 +0000

    Added Video Carousel for game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,6 +5,7 @@ class GamesController < ApplicationController
   def show
     
     @game_details = gamesRequest(params[:id])
+    
     if gameScreenshotRequest(params[:id]).empty?
       @game_screenshots = [{"id"=>244572, "game"=>55090, "height"=>720, "image_id"=>"iodd6zzceqq5jkufxpcz", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/iodd6zzceqq5jkufxpcz.jpg", "width"=>1280}, {"id"=>208211, "game"=>55090, "height"=>1080, "image_id"=>"wdsb42ukz39ywlzvhro4", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/wdsb42ukz39ywlzvhro4.jpg", "width"=>1920}, {"id"=>244573, "game"=>55090, "height"=>562, "image_id"=>"af8ueznswr8xpdkw9ukf", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/af8ueznswr8xpdkw9ukf.jpg", "width"=>1000}, {"id"=>208214, "game"=>55090, "height"=>1080, "image_id"=>"cxwpickwszhgdxvxdzzh", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/cxwpickwszhgdxvxdzzh.jpg", "width"=>1920}, {"id"=>208212, "game"=>55090, "height"=>1080, "image_id"=>"enex88ekm3se7a3vpqmp", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/enex88ekm3se7a3vpqmp.jpg", "width"=>1920}, {"id"=>208213, "game"=>55090, "height"=>1080, "image_id"=>"ywgv0zxrsocslwbkir8b", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/ywgv0zxrsocslwbkir8b.jpg", "width"=>1920}]
       else
@@ -12,7 +13,17 @@ class GamesController < ApplicationController
     end
      #Request for img_url and put into array
     puts @game_screenshots
-    # @game_videos = @game_details[] #Request for video_url and put into array
+
+    
+    if gamesVideoRequest(params[:id]).empty?
+      @game_videos = ["https://www.youtube.com/embed/LVIdmEfiFCk","https://www.youtube.com/embed/OAQm-EzbaHM"]
+    else
+      @game_videos = gamesVideoRequest(params[:id])
+    end
+    
+    @game_videos.each do |vid|
+      puts vid
+    end
     
     render 'games/show'
   end
