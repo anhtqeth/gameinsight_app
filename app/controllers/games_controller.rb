a5da3139ce81644cd38dff1449c2b25349e9470e
ethudev <anh.tq2411ps@gmail.com>
2019-06-07 10:53:27 +0000

    Add news feed logic and create news feed hashes for game. Add news feed carousel to game page. Close #9, #43

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,23 +1,34 @@
 class GamesController < ApplicationController
   include GamesApiModule
-  
-  #Render game detail url
+  #Render game detail page
   def show
-  #   results = Rails.cache.fetch(cache_key) do  # You could use a unique URL as part of the cache key
-  # # HTTP request
-  # # Return results from processing the response
-  #   end
     
-  #   @hash = Rails.cache.fetch('something', expires_in: 15.minutes) do
-  #   StatsClient.products
-  #   end
-    
-    # @game_details = gamesRequest(params[:id])
-    # @game_cover = gameCoverRequest(params[:id])
-    
-    @game_details = Rails.cache.fetch("#{params[:id]}/game_detail", expires_in: 1.month) do
-      gamesRequest(params[:id])
+    if(Rails.cache.fetch("#{params[:id]}/game_detail").nil?)
+        @game_details = Rails.cache.fetch("#{params[:id]}/game_detail", expires_in: 1.month) do
+          gamesRequest(params[:id])
+        end
+        game_collection_id = @game_details.first["collection"]
+        
+        @game_card_carousel_list = Rails.cache.fetch("#{game_collection_id}/game_series", expires_in: 1.month) do
+          gamesListProcess(game_series_ids)
+        end
+        @game_newsfeed_list = Rails.cache.fetch("#{params[:id]}/game_newsfeed", expires_in: 1.month) do
+          gameNewsFeedRequest(params[:id])
+        end
+    else
+        @game_details =  Rails.cache.fetch("#{params[:id]}/game_detail")
+        game_collection_id = @game_details.first["collection"]
+        @game_card_carousel_list = Rails.cache.fetch("#{game_collection_id}/game_series")
+        
+        if(Rails.cache.fetch("#{params[:id]}/game_newsfeed").nil?)
+          @game_newsfeed_list = Rails.cache.fetch("#{params[:id]}/game_newsfeed", expires_in: 1.month) do
+            gameNewsFeedRequest(params[:id])
+          end
+        else
+          @game_newsfeed_list = Rails.cache.fetch("#{params[:id]}/game_newsfeed")
+        end
     end
+    
     
     @game_cover = Rails.cache.fetch("#{params[:id]}/game_cover", expires_in: 1.month) do
       gameCoverRequest(params[:id])
@@ -27,17 +38,13 @@ class GamesController < ApplicationController
         gameScreenshotRequest(params[:id])
     end
     
-    #If There are no videos for the requested game, put up an example list
     @game_videos = Rails.cache.fetch("#{params[:id]}/game_videos", expires_in: 1.month) do
         gamesVideoRequest(params[:id])
     end
     
-    @dummy_games_news = [{"id"=>285391, "author"=>"Sal Romano", "category"=>15, "created_at"=>1509667200, "ignored"=>false, "image"=>"https://gematsu.com/wp-content/uploads/2017/11/Yakuza-Kiwami-2_2017_11-03-17_001.jpg", "published_at"=>1509667200, "pulse_image"=>107374, "pulse_source"=>15, "summary"=>"Sega has updated the official website for Yakuza: Kiwami 2 with information and screenshots of the Sotenbori, Osaka area. Get the details below. ■ Adventure ◆ Sotenbori, Osaka Many New Stores Appear! Kansai’s No. 1 Entertainment District: Sotenbori, Osaka Kansai’s greatest entertainment district, Sotenbori is a restaurant district representative of Osaka, where people spend too …", "tags"=>[1, 268435468, 268435487, 536871851, 536871945, 536872581, 536874146, 536894061, 536894062, 536894063, 536894064, 805361458, 1073741826], "title"=>"Yakuza: Kiwami 2 details Sotenbori, Osaka", "uid"=>"100122401e1aa339f5e530f4832996a3", "updated_at"=>1509667200, "website"=>107041}], [{"id"=>107041, "trusted"=>false, "url"=>"https://gematsu.com/2017/11/yakuza-kiwami-2-details-sotenbori-osaka"}]
+    #dummy_game_ids = [2268, 2269, 2271, 5316, 6440, 7075, 9692, 18181]
     
-    # if gamesNewsFeedRequest(params[:id]).nil?
-    #   @game_news = @dummy_games_new
-    # else
-    #   @game_news = gameNewsFeedRequest(params[:id])
+    @dummy_games_news = [{"id"=>285391, "author"=>"Sal Romano", "category"=>15, "created_at"=>1509667200, "ignored"=>false, "image"=>"https://gematsu.com/wp-content/uploads/2017/11/Yakuza-Kiwami-2_2017_11-03-17_001.jpg", "published_at"=>1509667200, "pulse_image"=>107374, "pulse_source"=>15, "summary"=>"Sega has updated the official website for Yakuza: Kiwami 2 with information and screenshots of the Sotenbori, Osaka area. Get the details below. ■ Adventure ◆ Sotenbori, Osaka Many New Stores Appear! Kansai’s No. 1 Entertainment District: Sotenbori, Osaka Kansai’s greatest entertainment district, Sotenbori is a restaurant district representative of Osaka, where people spend too …", "tags"=>[1, 268435468, 268435487, 536871851, 536871945, 536872581, 536874146, 536894061, 536894062, 536894063, 536894064, 805361458, 1073741826], "title"=>"Yakuza: Kiwami 2 details Sotenbori, Osaka", "uid"=>"100122401e1aa339f5e530f4832996a3", "updated_at"=>1509667200, "website"=>107041}], [{"id"=>107041, "trusted"=>false, "url"=>"https://gematsu.com/2017/11/yakuza-kiwami-2-details-sotenbori-osaka"}]
     
     render 'games/game_detail'
   end
--- lib/games_api_module.rb
+++ lib/games_api_module.rb
@@ -2,8 +2,21 @@ require 'uri'
 require 'net/http'
 module GamesApiModule
   
+  #Utils Constant
+  UNIX_TIME_NOW = Time.current.to_time.to_i
+  HTTP_CNF = Net::HTTP.new('api-v3.igdb.com', 80)
+  
+  DUMMY_SCREENSHOT = [[{"id"=>244572, "game"=>55090, "height"=>720, "image_id"=>"iodd6zzceqq5jkufxpcz", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/iodd6zzceqq5jkufxpcz.jpg", "width"=>1280}, {"id"=>208211, "game"=>55090, "height"=>1080, "image_id"=>"wdsb42ukz39ywlzvhro4", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/wdsb42ukz39ywlzvhro4.jpg", "width"=>1920}, {"id"=>244573, "game"=>55090, "height"=>562, "image_id"=>"af8ueznswr8xpdkw9ukf", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/af8ueznswr8xpdkw9ukf.jpg", "width"=>1000}, {"id"=>208214, "game"=>55090, "height"=>1080, "image_id"=>"cxwpickwszhgdxvxdzzh", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/cxwpickwszhgdxvxdzzh.jpg", "width"=>1920}, {"id"=>208212, "game"=>55090, "height"=>1080, "image_id"=>"enex88ekm3se7a3vpqmp", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/enex88ekm3se7a3vpqmp.jpg", "width"=>1920}, {"id"=>208213, "game"=>55090, "height"=>1080, "image_id"=>"ywgv0zxrsocslwbkir8b", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/ywgv0zxrsocslwbkir8b.jpg", "width"=>1920}]]
+  DUMMY_VIDEOS = ["https://www.youtube.com/embed/LVIdmEfiFCk","https://www.youtube.com/embed/OAQm-EzbaHM"]
+  DUMMY_GAME_IDS = [{"id"=>2948, "games"=>[2268, 2269, 2271, 5316, 6440, 7075, 9692, 18181, 20427, 23066, 25623, 26901, 36926, 42759, 43668, 58601, 61701, 61752, 65676, 69721, 78626, 78629, 78630, 78631, 78633, 113344]}]
+  
+  #NEED TO CATCH HTTP RESPONSE CODE BEFORE PROCEEDING!
+  #REFACTORING THE STRUCTURE OF CODE FOR REQUEST
+  
   #General Information
   GAME_URI = "https://api-v3.igdb.com/games/"
+  GAME_FRANCHISE_URI = "https://api-v3.igdb.com/franchises"
+  
   #Media (Videos/Photos)
   COVER_URI = 'https://api-v3.igdb.com/covers'
   ARTWORK_URI = 'https://api-v3.igdb.com/artworks'
@@ -19,7 +32,7 @@ module GamesApiModule
   #Searching 
   GAME_SEARCH_URI = "https://api-v3.igdb.com/search"
   GAME_COLLECTION_URI = "https://api-v3.igdb.com/collections"
-  GAME_FRANCHISE_URI = "https://api-v3.igdb.com/franchises"
+  
   
   #External Sites info
   GAME_NEWS_GROUP_URI = "https://api-v3.igdb.com/pulse_groups"
@@ -32,19 +45,9 @@ module GamesApiModule
   USERKEY = '049d27f7325bcb67768a30d5140fefb7'
   #anhtq2411 '049d27f7325bcb67768a30d5140fefb7' #EthuDev ada77f859e3e4c235b5b6e360c79e249
   
-  #Utils Constant
-  UNIX_TIME_NOW = Time.current.to_time.to_i
-  HTTP_CNF = Net::HTTP.new('api-v3.igdb.com', 80)
-  
-  DUMMY_SCREENSHOT = [[{"id"=>244572, "game"=>55090, "height"=>720, "image_id"=>"iodd6zzceqq5jkufxpcz", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/iodd6zzceqq5jkufxpcz.jpg", "width"=>1280}, {"id"=>208211, "game"=>55090, "height"=>1080, "image_id"=>"wdsb42ukz39ywlzvhro4", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/wdsb42ukz39ywlzvhro4.jpg", "width"=>1920}, {"id"=>244573, "game"=>55090, "height"=>562, "image_id"=>"af8ueznswr8xpdkw9ukf", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/af8ueznswr8xpdkw9ukf.jpg", "width"=>1000}, {"id"=>208214, "game"=>55090, "height"=>1080, "image_id"=>"cxwpickwszhgdxvxdzzh", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/cxwpickwszhgdxvxdzzh.jpg", "width"=>1920}, {"id"=>208212, "game"=>55090, "height"=>1080, "image_id"=>"enex88ekm3se7a3vpqmp", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/enex88ekm3se7a3vpqmp.jpg", "width"=>1920}, {"id"=>208213, "game"=>55090, "height"=>1080, "image_id"=>"ywgv0zxrsocslwbkir8b", "url"=>"//images.igdb.com/igdb/image/upload/t_1080p/ywgv0zxrsocslwbkir8b.jpg", "width"=>1920}]]
-  DUMMY_VIDEOS = ["https://www.youtube.com/embed/LVIdmEfiFCk","https://www.youtube.com/embed/OAQm-EzbaHM"]
-#NEED TO CATCH HTTP RESPONSE CODE BEFORE PROCEEDING!
-#REFACTORING THE STRUCTURE OF CODE FOR REQUEST
-
-  
-
 
   def gamesRequest(game_id)
+    puts "Called to Game Request with parameter: " << game_id.to_s
     request = Net::HTTP::Get.new(URI(GAME_URI), {'user-key' => USERKEY})
   
     #request.body = "fields *; where id = #{game_id};";
@@ -54,45 +57,82 @@ module GamesApiModule
     JSON.parse(response.read_body)
   end
   
+   #This is used to query for  Games Series,
+  #result of collection are based on gamesSearchRequest
+  def gamesSeriesRequest(collection_id)
+    puts "Called to Game Series Request with parameter: " << collection_id.to_s
+    request = Net::HTTP::Get.new(URI(GAME_COLLECTION_URI), {'user-key' => USERKEY})
+    request.body = "fields games; where id = (#{collection_id}); limit 5;"
+    result= JSON.parse(HTTP_CNF.request(request).read_body)
+    result.first["games"] 
+  end
+  
+  def gameFranchiseRequest(game_id)
+    request = Net::HTTP::Get.new(URI(GAME_FRANCHISE_URI), {'user-key' => USERKEY})
+    request.body = "fields *; where games = #{game_id};";
+    response = HTTP_CNF.request(request)
+    
+    JSON.parse(response.read_body)
+  end
+  
   def gameNewsFeedRequest(game_id)
+    puts "Called to News Feed Request with parameter: " << game_id.to_s
     request = Net::HTTP::Get.new(URI(GAME_NEWS_GROUP_URI), {'user-key' => USERKEY})
     request.body = "fields *; where game = #{game_id};"
     response = HTTP_CNF.request(request)
     result = JSON.parse(response.read_body)
-    puts result
-    article_list = []
-    result.each do |rs|
-      article_list << rs["pulses"]
-    end
-    puts 'List of pulses: ' 
-    puts article_list
- 
     
-    article_list.each do |arc|
-      gameArticleRequest(arc)
+    article_ids = []
+    result.each do |rs|
+      article_ids << rs["pulses"].join.to_i
     end
-    article_list
+    
+    puts "This is the list of article ID: " << article_ids[0..4].to_s
+    article_ids[0..4]
+    game_article_list = []
+    article_ids[0..4].each do |id|
+      game_article = gameArticleRequest(id)
+      game_article_list << game_article
+    end
+    game_article_list
   end
-  
+    
   def gameArticleRequest(id)
+    puts "Called to Game Article Request with parameter: " << id.to_s
     request = Net::HTTP::Get.new(URI(GAME_ARTICLE_URI), {'user-key' => USERKEY})
     request.body = "fields *; where id = #{id};"
-    
     response = HTTP_CNF.request(request)
-    article_meta = JSON.parse(response.read_body)
-    article_url = gameArticleExternalRequest(article_meta.first["website"])
-    return [article_meta,article_url]
+    
+    game_article = {:id =>nil,:author=>nil,:summary=> nil,:img=>nil,:created_at=>nil,:title=>nil,:url=>nil}
+    article_meta = JSON.parse(response.read_body).first
+    
+    #Constructing Article Hashes
+    article_id = article_meta['id']
+    game_article.store(:id,article_id)
+    article_author = article_meta['name']
+    game_article.store(:author,article_author)
+    article_summary = article_meta['summary']
+    game_article.store(:summary,article_summary)
+    article_img = article_meta['image']
+    game_article.store(:img,article_img)
+    article_created_at = article_meta['created_at']
+    game_article.store(:created_at,article_created_at)
+    article_title = article_meta['title']
+    game_article.store(:title,article_title)
+    article_url = gameArticleExternalRequest(article_meta["website"])
+    game_article.store(:url,article_url)
+    
+    game_article
   end
   
   def gameArticleExternalRequest(id)
+    puts "Called to Game Article External Request with parameter: " << id.to_s
     request = Net::HTTP::Get.new(URI(GAME_EXTERNAL_ARTICLE_URI), {'user-key' => USERKEY})
     request.body = "fields *; where id = #{id};"
     
     response = HTTP_CNF.request(request)
-    JSON.parse(response.read_body)
+    JSON.parse(response.read_body).first["url"]
   end
-  
-  
 
   def gamesPlatformLogoRequest(logo_id)
     request = Net::HTTP::Get.new(URI(PLATFORM_LOGO), {'user-key' => USERKEY})
@@ -125,6 +165,7 @@ module GamesApiModule
   end
   
   def gameReleaseDateRequest
+    puts "Called to Release Date Request with parameter: "
     request = Net::HTTP::Get.new(URI(RELEASE_URI), {'user-key' => USERKEY})
     request.body = "fields *; where date > #{UNIX_TIME_NOW}  & platform = 48;"
     response = HTTP_CNF.request(request)
@@ -138,10 +179,11 @@ module GamesApiModule
     result
   end
       
-  def gameCoverRequest(gameID)
+  def gameCoverRequest(game_id)
+      puts "Called to Game Cover Request with parameter: " << game_id.to_s
       request = Net::HTTP::Get.new(URI(COVER_URI), {'user-key' => USERKEY})
       #request.body = "fields name,summary; where id = #{gameID};";
-      request.body = "fields url; where game = (#{gameID});"
+      request.body = "fields url; where game = (#{game_id});"
       response = HTTP_CNF.request(request)
       
       if JSON.parse(response.read_body).empty?
@@ -152,9 +194,10 @@ module GamesApiModule
       end
   end
   
-  def gameArtworkRequest(gameID)
+  def gameArtworkRequest(game_id)
+      puts "Called to Game Artwork Request with parameter: " << game_id.to_s
       request = Net::HTTP::Get.new(URI(ARTWORK_URI), {'user-key' => USERKEY})
-      request.body = "fields *; where game = (#{gameID});"
+      request.body = "fields *; where game = (#{game_id});"
       response = HTTP_CNF.request(request)
       
       if JSON.parse(response.read_body).empty?
@@ -166,12 +209,9 @@ module GamesApiModule
       end
   end
 
-  def genericGameRequest #This was firstly created to randomly pick a game details
-    
-  end
-  
   #This is used to search for a specific game. Initially, this just a hardcoded string to return details of a game
   def gamesSearchRequest(game_string)
+    puts "Called to Game Search Request with parameter: " << game_string
     request = Net::HTTP::Get.new(URI(GAME_URI), {'user-key' => USERKEY})
     #request.body = 'fields *; where name = "' << game_string << '";' QUERY FOR GAME_SEARCH_URI
     request.body  = 'search " '<< game_string << '";'
@@ -186,17 +226,10 @@ module GamesApiModule
     game_ids
   end
   
-  #This is used to query for  Games Series,
-  #result of collection are based on gamesSearchRequest
-  def gamesSeriesRequest(collection_id)
-    request = Net::HTTP::Get.new(URI(GAME_COLLECTION_URI), {'user-key' => USERKEY})
-    request.body = "fields games; where id = (#{collection_id}); limit 5;"
-    JSON.parse(HTTP_CNF.request(request).read_body)
-  end
-  
-  def gameScreenshotRequest(gameID)
+  def gameScreenshotRequest(game_id)
+      puts "Called to Game Screenshots Request with parameter: " << game_id.to_s
       request = Net::HTTP::Get.new(URI(SCREENSHOTS_URI), {'user-key' => USERKEY})
-      request.body = "fields *; where game = (#{gameID});"
+      request.body = "fields *; where game = (#{game_id});"
       response = HTTP_CNF.request(request)
   
       if JSON.parse(response.read_body).empty?
@@ -210,8 +243,8 @@ module GamesApiModule
       end
   end
   
-  
   def gamesVideoRequest(game_id)
+    puts "Called to Game Video Request with parameter: " << game_id.to_s
     request = Net::HTTP::Get.new(URI(VIDEO_URI), {'user-key' => USERKEY})
     request.body = "fields *; where game = (#{game_id});"
     response = HTTP_CNF.request(request)
@@ -229,14 +262,14 @@ module GamesApiModule
           end
           game_videos_url
     end
-    
   end
   
   #
   def gamesListProcess(games_id_array)
+    puts "Called to Game List Request with parameter: " << games_id_array.to_s
     game_card_list = []
     
-    games_id_array[0...4].each do |x|
+    games_id_array[0..7].each do |x|
       #game_card = {:id =>nil,:name=>nil, :summary=> nil,:cover=>nil}
       
       game_card = {:id =>nil,:name=>nil, :summary=> nil,:cover=>nil,:first_release_date=>nil,:platform=>nil,:genres=>nil}
@@ -249,8 +282,6 @@ module GamesApiModule
       game_card.store(:summary,game_summary)
       game_cover = gameCoverRequest(x)
       game_card.store(:cover,game_cover)
-      
-      
       
       unless (game_detail["platforms"].nil? or game_detail["genres"].nil? or game_detail["first_release_date"].nil? )
         game_platform = game_detail["platforms"].map{|x| x["name"]}.join(', ')
