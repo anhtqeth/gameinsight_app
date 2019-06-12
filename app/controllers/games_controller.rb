200447c92023286dd3c75108d728f44f3d194303
ethudev <anh.tq2411ps@gmail.com>
2019-06-12 08:56:32 +0000

    Added pagination with will paginate, modify initializer to include array pagination. Still need styling on pagination. Close #49

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -51,7 +51,7 @@ class GamesController < ApplicationController
       game_id_result = Rails.cache.fetch("#{params[:name]}/game_name_search", expires_in: 1.month) do
         gamesSearchRequest(params[:name])
       end
-      @game_card_result = gamesListProcess(game_id_result)
+      @game_card_result = gamesListProcess(game_id_result).paginate(:page =>params[:page], :per_page => 4)   
       render 'games/search_result'
     end
   end
