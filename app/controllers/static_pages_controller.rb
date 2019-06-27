f0c879213d97fcd024d54ece9ce8450d0a6541e8
ethudev <anh.tq2411ps@gmail.com>
2019-06-27 04:42:13 +0000

    Minor change to build request code

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -34,7 +34,6 @@ class StaticPagesController < ApplicationController
     end
     #Currently set in controller, better if user can pick this from the view. Will change in 0.3
     time = (Time.current - 6.days).to_time.to_i
-    
     @latest_newsfeed = gameLatestNewsRequest(time) # = Rails.cache.fetch("latest/news_feed", expires_in: 7.days) do
       
    # end
