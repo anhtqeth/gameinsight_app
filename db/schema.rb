5bdedc25ec4acdcf8d1e2a0299f43d4592fb5dec
ethudev <anh.tq2411ps@gmail.com>
2019-07-27 14:09:39 +0000

    Adde HABTM for genres and game. Add basic Rspec test for game genres

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_083418) do
+ActiveRecord::Schema.define(version: 2019_07_27_133534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2019_07_22_083418) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "game_genres_games", id: false, force: :cascade do |t|
+    t.bigint "game_id", null: false
+    t.bigint "game_genre_id", null: false
+    t.index ["game_genre_id"], name: "index_game_genres_games_on_game_genre_id"
+    t.index ["game_id"], name: "index_game_genres_games_on_game_id"
   end
 
   create_table "game_videos", force: :cascade do |t|
