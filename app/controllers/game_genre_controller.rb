dde857c171d34bed0c2a6543c7ae60b1f622d661
ethudev <anh.tq2411ps@gmail.com>
2019-07-29 07:35:29 +0000

    Added code to display rendered html rich text content on view

--- app/controllers/game_genre_controller.rb
+++ app/controllers/game_genre_controller.rb
@@ -1,7 +1,10 @@
 class GameGenreController < ApplicationController
+  include ContentModule
   
   def show
+    @genre_detail = renderRichText
     
+    render 'game_genre/game_genre_detail'
   end
   
   
