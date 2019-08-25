affd08e32c0015ef747b5d57d3f3281ca9affad0
ethudev <anh.tq2411ps@gmail.com>
2019-08-25 08:06:11 +0000

    Added workking carousel with overlay renderer to Games Helper

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -15,23 +15,6 @@ module GamesHelper
   end
   
   
-  def getMediaUrl(model,data_type)
-    media = []
-    model.each do |game|
-      case data_type
-      when 'vid'
-        next if game.videos.empty?
-        media << game.game_videos.first.url
-      when 'img'
-        next if game.screenshots.empty?
-        media << game.screenshots.first.url
-      else
-        puts 'Please set correct data type'
-      end
-    end
-    media
-  end
-  
   #Newsfeed render for a specific game
   #Input is newsfeed from controller
   #If data is nil, render a simple content tag.
@@ -71,17 +54,74 @@ module GamesHelper
     end
   end
   
-  
+   
   #Used to render bootstrap carousel
   #This include a small div overlay on top of the image
   #and show text on the image
-  def carousel_render(data,type)
+  def carousel_render(data)
     if data_verify(data)
-      Carousel.new(self,getMediaUrl(data,type)).html
+      #Carousel.new(self,getMediaUrl(data,type)).html
+      Carousel.new(self,data).html
     else
       content_tag(:p, "We found no news source related to this game yet. 
       Do you have ones? Consider contributing it!")
     end
+  end
+  
+  # <a href="#" class="badge badge-primary">Primary</a>
+  #TODO Add href to game genres url
+  
+  def getMediaUrl(model,data_type)
+    media = []
+    model.each do |game|
+      case data_type
+      when 'vid'
+        next if game.videos.empty?
+        media << game.game_videos.first.url
+      when 'img'
+        next if game.screenshots.empty?
+        media << game.screenshots.first.url
+      else
+        puts 'Please set correct data type'
+      end
+    end
+    media
+  end
+  
+  def badge_render(data)
+    if data_verify(data)
+      content = []
+      data.each do |x|
+        content << content_tag(:span,x.name,class: "badge badge-warning") 
+      end
+      safe_join(content)
+    else
+      puts 'No genre specify'
+    end  
+  end
+  
+  #Render an overlay when needed
+  #CSS of overlay will be handled on game.scss
+  def overlay_render(id,data)
+    if data_verify(data)
+      content = []
+      content << content_tag(:div,card_render(data),class: "card-img-overlay",id: id)
+      safe_join(content)
+    else
+      puts "Invalid Data!"
+    end
+  end
+  
+  #Used to render a card only.
+  #TODO - Refactor to make this more generic and can be used again
+  def card_render(game_data)
+    content = []
+    content << content_tag(:h1,game_data.name, class: "card-title")
+    content << content_tag(:p,truncate(game_data.summary,length: 300), class: "card-text")
+    content << content_tag(:p,date_format(game_data.first_release_date),class: "card-text")
+    content << badge_render(game_data.game_genres)
+    content << content_tag(:a,"Details",class: "btn btn-dark", id: "discover-btn", "href": game_path(game_data.slug))
+    safe_join(content)
   end
   
   #Carousel class for handling Carousel
@@ -92,14 +132,16 @@ module GamesHelper
     #   @view, @images = view, images
     #   @uid = SecureRandom.hex(6)
     # end
-    
     #TODO - The id is hard-coded! This still cannot be used widely!
-    def initialize(view,media)
-      @view, @media_data, @size = view,media,media.size
+    def initialize(view,data)
+      @media_data = view.getMediaUrl(data,'img')
+      @size = @media_data.size
+      @view, @data = view,data
+      #@view, @media_data, @size = view,media,media.size
     end
     
     def html
-      carousel = safe_join([carousel_indicator(@size),carousel_inner(@media_data)])
+      carousel = safe_join([carousel_indicator(@size),carousel_inner(@media_data,@data)])
       content_tag(:div,carousel,id: "carouselIndicators",class: "carousel slide","data-ride": "carousel")
     end
     
@@ -126,24 +168,48 @@ module GamesHelper
       safe_join(content)
     end
     
-    def carousel_inner(media_data)
-      content = safe_join([carousel_item(media_data),carousel_control])
+    def carousel_inner(media_data,data)
+      content = safe_join([carousel_item(data),carousel_control])
       content_tag(:div,content,class: "carousel-inner")
     end
     
     #Render carousel-item div. 
     #Input is an array of media_url
-    def carousel_item(media_data)
+    
+    # def carousel_item(media_data,data)
+    #   items = []
+    #   #Render the first img (active) media
+    #   first_media = image_tag(media_data.first, class: "d-block w-100", id: "hot-games")
+    #   items << content_tag(:div, first_media, class: "carousel-item active")
+    #   items << view.overlay_render("game-discover-overlay",data[0])
+      
+    #   data[1..-1].each do |game|
+    #     remaining_media = []
+    #     media_data[1..-1].each do |media|
+    #       remaining_media << image_tag(media, class: "d-block w-100",  id: "hot-games")
+    #       remaining_media << view.overlay_render("game-discover-overlay",game)
+    #     end
+    #     items << content_tag(:div, safe_join(remaining_media), class: "carousel-item")
+    #   end
+      
+    #   safe_join(items)
+    # end
+    
+    def carousel_item(data)
       items = []
       #Render the first img (active) media
-      first_media = image_tag(media_data.first, class: "d-block w-100", id: "hot-games")
+      first_media = image_tag(data.first.screenshots.first.url, class: "d-block w-100", id: "hot-games")
       items << content_tag(:div, first_media, class: "carousel-item active")
+      items << view.overlay_render("game-discover-overlay",data[0])
       
-      media_data[1..-1].each do |media_url|
-        next if media_url.nil?
-        remaining_media = image_tag(media_url, class: "d-block w-100",  id: "hot-games")
-        items << content_tag(:div, remaining_media, class: "carousel-item")
+      data[1..-1].each do |game|
+        next if game.screenshots.empty?
+        remaining_media = []
+        remaining_media << image_tag(game.screenshots.first.url, class: "d-block w-100",  id: "hot-games")
+        remaining_media << view.overlay_render("game-discover-overlay",game)
+        items << content_tag(:div, safe_join(remaining_media), class: "carousel-item")
       end
+      
       safe_join(items)
     end
     
