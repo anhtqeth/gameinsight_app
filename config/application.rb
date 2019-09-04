7e540fd5739cfaaa7df08a3fbed5881dd16193ee
ethudev <anh.tq2411ps@gmail.com>
2019-09-04 15:09:48 +0000

    Added translation text, added ebs configuration for deployment to AWS

--- config/application.rb
+++ config/application.rb
@@ -14,10 +14,8 @@ module EthuGamedbApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    
-    config.i18n.available_locales = [:en, :vie]
-    config.i18n.default_locale = :en
-    
+    config.i18n.load_path += Dir["#{Rails.root}/plugins/*/config/locales/*.yml"]
+   
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,40 @@
 Rails.application.routes.draw do
   
-  root 'static_pages#home'
-
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  get '/release_by_platform', to: 'static_pages#latest_releases', as: 'release'
+  # scope "(:locale)", "locale: /#{I18n.available_locales.join("|")}/" do
+    
+  # end
   
-  get  '/signup',  to: 'user#new'
+ 
+  
+  scope "(:locale)", locale: /en|vi/ do
+    root 'static_pages#home'
+    get  '/help',    to: 'static_pages#help'
+    get  '/about',   to: 'static_pages#about'
+    get  '/contact', to: 'static_pages#contact'
+    get '/release_by_platform', to: 'static_pages#latest_releases', as: 'release'
+  
+    get  '/signup',  to: 'user#new'
   
   #GamesController
   #get '/games/:id', to: 'games#show', as: 'game'
-  get '/games/(/:id)(/:external_id)', to: 'games#show', as: 'game'
+    get '/games/(/:id)(/:external_id)', to: 'games#show', as: 'game'
   
-  get 'games/new-releases', to: 'games#newrelease'
-  get 'games/:id/guides', to: 'games#guides'
+    get 'games/new-releases', to: 'games#newrelease'
+    get 'games/:id/guides', to: 'games#guides'
   
-  get '/genres/', to: 'game_genre#show', as: 'game_genres'
+    get '/genres/', to: 'game_genre#show', as: 'game_genres'
   
-  get '/hot', to: 'static_pages#hot'
-  get '/discover', to: 'games#discover'
+    get '/hot', to: 'static_pages#hot'
+    get '/discover', to: 'games#discover'
   
   #PlatformsController
-  get '/platforms/:id', to: 'platforms#show', as: 'platform'
+    get '/platforms/:id', to: 'platforms#show', as: 'platform'
   
-  #??Controller
-  get '/reviews', to: 'reviews#show'
+    #??Controller
+    get '/reviews', to: 'reviews#show'
   
-  get '/search/', to: 'games#find', as: 'search'
+    get '/search/', to: 'games#find', as: 'search'
   
+    end
+ 
 end
