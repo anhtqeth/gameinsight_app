7664102661b082a481d1efa6f960675ec688cc89
ethudev <anh.tq2411ps@gmail.com>
2019-09-04 15:09:48 +0000

    Added translation text, added ebs configuration for deployment to AWS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def home
-    render html: "Welcome to GameInsight"
+  before_action :set_locale
+  
+  def default_url_options
+    { locale: I18n.locale }
   end
+    
+  def set_locale
+    I18n.locale = extract_locale || I18n.default_locale
+  end
+    
+  def extract_locale
+    parsed_locale = params[:locale]
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+  
+  # def home
+  #   render html: "Welcome to GameInsight"
+  # end
 end
