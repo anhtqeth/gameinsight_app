35c122189e202df425c02b911f0e7fb5b1e4c236
ethudev <anh.tq2411ps@gmail.com>
2019-09-09 08:50:48 +0000

    Added session controller. Added session helper methods to faciliate login/out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
+  include SessionsHelper #Include session helper to faciliate user login
   
   def default_url_options
     { locale: I18n.locale }
