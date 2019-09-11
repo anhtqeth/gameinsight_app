cf9a8e5e4a89fe9c5347014ca6dbacf9752acf05
ethudev <anh.tq2411ps@gmail.com>
2019-09-11 03:35:56 +0000

    Created Edit Page for game. Admin can now check details on the Games Management screen. Edit worked. Close #93

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -16,10 +16,24 @@ class GamesController < ApplicationController
   
   def destroy
     Game.friendly.find(params[:id]).destroy
-    flash[:success] = "Game deleted!"
+    flash.now[:success] = "Game deleted!"
     redirect_back(fallback_location: root_path)
   end
   
+  def edit
+    @game = Game.friendly.find(params[:id])
+  end
+
+  def update
+    #Should show assocciated screenshots and videos for update
+    @game = Game.friendly.find(params[:id])
+    if @game.update_attributes(game_params)
+      flash.now[:success] = "Game detail updated"
+      redirect_to @game
+    else
+      render 'edit'
+    end
+  end
  
   def show
     #If this game is not in db, save it. 
@@ -119,16 +133,17 @@ class GamesController < ApplicationController
     
     
   end
-  
-  def update
-    #Should show all current game detail in db 
-    #Should show assocciated screenshots and videos for update
-  end
- 
-  def logged_in_user
+
+  private 
+    def game_params
+      params.require(:game).permit(:cover, :name,
+                                   :slug,:summary, :storyline, :first_release_date)
+    end
+    
+    def logged_in_user
       unless logged_in?
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
-  end
+    end
 end
