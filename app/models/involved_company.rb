3fe267a3449c3303eefd9ce94dd3c2cfb70b50b1
ethudev <anh.tq2411ps@gmail.com>
2019-09-24 15:30:17 +0000

    Modified scope to class method in involved company

--- app/models/involved_company.rb
+++ app/models/involved_company.rb
@@ -6,10 +6,21 @@ class InvolvedCompany < ApplicationRecord
   
   #Scope was used to assgne active record query on certain circumstances
   #For example, below added "AND publisher = true" to the end of the query
-  #
-  scope :publisher, -> { find_by(publisher: true) }
-  scope :developer, -> { find_by(developer: true) }
+ 
   scope :supporter, -> { find_by(supporting: true) }
   scope :porter, -> { find_by(porting: true) }
+  
+  def self.publisher
+    publisher_company = self.find_by(publisher: true)
+    publisher_company.present? ? publisher_company : nil
+  end  
+  
+  def self.developer
+    developer_company = self.find_by(developer: true)
+    developer_company.present? ? developer_company : nil
+  end  
+  
+  #cp = Game.find(64)  cp.involved_companies.developer
+  
   
 end
