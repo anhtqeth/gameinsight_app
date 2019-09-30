024c69ef5f58cbc2d596a57fd079f2646e179a9a
ethudev <anh.tq2411ps@gmail.com>
2019-09-30 10:52:16 +0000

    Fixed ort to :port

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,13 +6,12 @@ redis_config.merge! redis_config.fetch(Rails.env, {})
 redis_config.symbolize_keys!
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[ort]}/12" }
+  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[ort]}/12" }
+  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
 end
-
 
 
 # Old configuration in dev
