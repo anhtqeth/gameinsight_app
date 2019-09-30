f409b6dba6a4e6b50ade4a4ce8cae0ed2d387553
ethudev <anh.tq2411ps@gmail.com>
2019-09-30 06:16:12 +0000

    Added sidekiq gem to gemfile. Generated FetchGamesCollectionJob

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,10 @@ require_relative 'boot'
 require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+# Settings in config/environments/* take precedence over those specified here.
+# Application configuration can go into files in config/initializers
+# -- all .rb files in that directory are automatically loaded after loading
+# the framework and any gems in your application.
 Bundler.require(*Rails.groups)
 
 module EthuGamedbApp
@@ -10,12 +14,9 @@ module EthuGamedbApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.eager_load_paths << Rails.root.join('lib')
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-    config.i18n.load_path += Dir["#{Rails.root}/plugins/*/config/locales/*.yml"]
    
+    config.i18n.load_path += Dir["#{Rails.root}/plugins/*/config/locales/*.yml"]
+    config.active_job.queue_adapter = :sidekiq 
   end
 end
 
