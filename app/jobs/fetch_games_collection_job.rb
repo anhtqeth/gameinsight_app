f5b0d3c09f6fbad4c7cb937d3b1c4ce91ebe603e
ethudev <anh.tq2411ps@gmail.com>
2019-09-30 07:22:39 +0000

    Working sidekiq and redis. Fixed an issue with params[:id] (slug vs integer)

--- app/jobs/fetch_games_collection_job.rb
+++ app/jobs/fetch_games_collection_job.rb
@@ -1,14 +1,17 @@
 class FetchGamesCollectionJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    game_collection = GameCollection.new
-      
-    if game_collection.saveAPIData(game.external_id).nil?
-      @game_card_carousel_list = nil
+  def perform(game)
+    if game.game_collection.nil?
+      game_collection = GameCollection.new
+      if game_collection.saveAPIData(game.external_id).nil?
+        @game_card_carousel_list = nil
+      else
+        @game_card_carousel_list = game.game_collection.games
+        @size = @game_card_carousel_list.size/2 #TODO - Move this to helper
+      end
     else
-      @game_card_carousel_list = game.game_collection.games
-      @size = @game_card_carousel_list.size/2 #TODO - Move this to helper
+      puts "Already updated collection"
     end
   end
 end
