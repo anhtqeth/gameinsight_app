2d66880ffdd84c01804a9daa1d089012f387f16b
ethudev <anh.tq2411ps@gmail.com>
2019-10-10 07:07:37 +0000

    Added newsfeed helper for rendering card base on newsfeed data

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -22,19 +22,31 @@ include ApplicationHelper
   #<div class="shadow p-3 mb-5 bg-white rounded">Regular shadow</div>
   #Used to render rssfeed for an article list
   def rssFeedrenderer(rss_data)
-    if data_verify(data)
+    if data_verify(rss_data)
+      html = []
       rss_data.each do |arc|
-        html = []
         
-        content_tag(:div,)
-        
-        
+        html << content_tag(:div,cardRenderer(arc), class:"col-md-4")
       end
-      
+      content_tag(:div,content_tag(:div,safe_join(html),class:"row"),class: "container")
     else
       
     end
     
+  end
+  
+  def cardRenderer(data)
+    card_body = []  
+    unless data[:img].nil?
+      card_body << image_tag(data[:img], class: "card-img-top")
+    end
+    card_body << content_tag(:hr)
+    card_body << content_tag(:h5,data[:title] ,class:"card-title")
+    card_body << content_tag(:p,truncate(data[:summary], length: 200),class:"card-text")
+    card_body << content_tag(:hr)
+    card_body << link_to("View More", data[:url],target: "_blank", rel: "nofollow")
+    card_body << content_tag(:p,content_tag(:small,'Last updated ' << time_ago_in_words(data[:publish_at]),class:"text-muted"),class:"card-text")
+    content_tag(:div,content_tag(:div,safe_join(card_body) ,class:"card-body"),class:"card shadow p-3 mb-3")
   end
   
   
