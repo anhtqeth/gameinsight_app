65d7cdd1259650e98ea9e834b3e13a2010bc93ea
ethudev <anh.tq2411ps@gmail.com>
2019-11-08 10:53:45 +0000

    Modified configuration for sidekiq url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,18 +1,34 @@
 # frozen_string_literal: true
 
-rails_root = Rails.root || File.dirname(__FILE__) + '/../..'
-rails_env = Rails.env || 'development'
+# rails_root = Rails.root || File.dirname(__FILE__) + '/../..'
+# rails_env  = Rails.env || 'development'
 
-redis_config = YAML.load_file(rails_root.to_s + '/config/redis.yml')
-redis_config.merge! redis_config.fetch(Rails.env, {})
-redis_config.symbolize_keys!
+# redis_config = YAML.load_file(rails_root.to_s + '/config/redis.yml')
+# redis_config.merge! redis_config.fetch(Rails.env, {})
+# redis_config.symbolize_keys!
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
+# end
+
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
+# end
+
+if Rails.env.development?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: 'redis://localhost:6379' }
+  end
 end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
+if Rails.env.production?
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['REDISTOGO_URL'], size: 2 }
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['REDISTOGO_URL'], size: 20 }
+  end
 end
 
 # Old configuration in dev
